1bb753bef3d8cb79ccc8e2c3eca5295b508046db
peterlau123 <freelaupeter@gmail.com>
2025-11-11 00:26:45 +0800

    fix: fix by addding logging definition in log.h

--- include/NovaLLM/utils/log.h
+++ include/NovaLLM/utils/log.h
@@ -1,14 +1,22 @@
 #pragma once
+
+// Logger wrapper:
+//  - If NOVA_LLM_ENABLE_LOGGING is defined and spdlog is available, use spdlog.
+//  - Otherwise provide a no-op Logger and a minimal spdlog::level::level_enum so callers compile.
+
+#include <string>
+#include <memory>
+
+// Prefer spdlog when logging is enabled and available
+#if defined(NOVA_LLM_ENABLE_LOGGING) && NOVA_LLM_ENABLE_LOGGING && __has_include(<spdlog/spdlog.h>)
+
 #if __has_include(<fmt/ostream.h>)
 #  include <fmt/ostream.h>
 #elif __has_include(<spdlog/fmt/ostr.h>)
 #  include <spdlog/fmt/ostr.h>
 #endif
 #include <spdlog/spdlog.h>
-
 #include <filesystem>
-#include <memory>
-#include <string>
 
 namespace nova_llm {
 
@@ -24,9 +32,7 @@ class Logger {
             spdlog::level::level_enum level = spdlog::level::info);
 
   void setLevel(spdlog::level::level_enum level) {
-    if (logger_) {
-      logger_->set_level(level);
-    }
+    if (logger_) logger_->set_level(level);
   }
 
   template <typename... Args>
@@ -74,7 +80,64 @@ class Logger {
 
 }  // namespace nova_llm
 
-// Convenience macros
+#else
+
+// spdlog not available or logging disabled -> provide no-op Logger
+
+// Minimal spdlog::level namespace so callers using spdlog::level::info still compile
+namespace spdlog {
+namespace level {
+enum level_enum { trace = 0, debug = 1, info = 2, warn = 3, err = 4, critical = 5 };
+}  // namespace level
+}  // namespace spdlog
+
+namespace nova_llm {
+
+class Logger {
+ public:
+  static Logger& getInstance() {
+    static Logger instance;
+    return instance;
+  }
+
+  void init(const std::string& /*name*/ = "NovaLLM",
+            const std::string& /*logFile*/ = "NovaLLM.log",
+            spdlog::level::level_enum /*level*/ = spdlog::level::info) {}
+
+  void setLevel(spdlog::level::level_enum /*level*/) {}
+
+  template <typename... Args>
+  void trace(const char* /*fmt*/, const Args&... /*args*/) {}
+
+  template <typename... Args>
+  void debug(const char* /*fmt*/, const Args&... /*args*/) {}
+
+  template <typename... Args>
+  void info(const char* /*fmt*/, const Args&... /*args*/) {}
+
+  template <typename... Args>
+  void warn(const char* /*fmt*/, const Args&... /*args*/) {}
+
+  template <typename... Args>
+  void error(const char* /*fmt*/, const Args&... /*args*/) {}
+
+  template <typename... Args>
+  void critical(const char* /*fmt*/, const Args&... /*args*/) {}
+
+  void flush() {}
+
+ private:
+  Logger() = default;
+  ~Logger() = default;
+  Logger(const Logger&) = delete;
+  Logger& operator=(const Logger&) = delete;
+};
+
+}  // namespace nova_llm
+
+#endif
+
+// Convenience macros (same API in both branches)
 #define LOG_TRACE(...) nova_llm::Logger::getInstance().trace(__VA_ARGS__)
 #define LOG_DEBUG(...) nova_llm::Logger::getInstance().debug(__VA_ARGS__)
 #define LOG_INFO(...) nova_llm::Logger::getInstance().info(__VA_ARGS__)
